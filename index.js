323d17ce0b655fd4c37d914333ab336916eb562c
angelasubi <21978301@qq.com>
2018-07-30 19:34:06 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-(function(global, factory) {
-  typeof module === 'object' && typeof module.exports === 'object' ? module.exports = global.document ? factory(global, true) : factory(this) : factory(global)
-})(typeof window !== 'undefined' ? window : this, function(window) {
-
-})
+;(function(ele, factory) {
+  if (typeof exports === 'object') {
+    module.exports = factory(ele)
+  } else if (typeof define === 'function' && define.amd) {
+    define([], factory(ele))
+  } else {
+    ele.LazyLoad = factory(ele)
+  }
+}(typeof global !== 'undefined' ? global : this.window || this.global, function(ele) {
+  'use strict'
+  const defaultOption = {
+    src: 'data-src',
+    selecotr: '.lazyload'
+  }
+}))
