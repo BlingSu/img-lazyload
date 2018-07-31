1fb400f2a513391540b31fd843670fbbee652fe3
angelasubi <21978301@qq.com>
2018-07-31 14:54:40 +0800

    feat: lazyload

--- index.js
+++ index.js
@@ -10,6 +10,45 @@
   'use strict'
   const defaultOption = {
     src: 'data-src',
-    selecotr: '.lazyload'
+    selector: '.lazyload'
   }
+  let indexNum = 0
+  function LazyLoad(selector, option) {
+    this.selector = selector
+    this.option = option || {}
+    this.indexNum = indexNum
+    this.init()
+  }
+  LazyLoad.prototype = {
+    init: function() {
+      window.onload = function() {
+        this.checkImages()
+      }
+    },
+    isShow: function(selector) {
+      const bound = selector.getBoundingClientRect()
+      const clientHeight = window.innerHeight || document.documentElement.clientHeight // in bottom
+      console.log(bound, clientHeight)
+      return bound.bottom > 0 && bound.top < clientHeight
+    },
+    loadImage: function(selector) {
+      let image = new Image()
+      image.onload = function() {
+        selector.src = image.src
+      }
+      image.src = selector.dataset.src
+      selector.dataset.src = ''
+    },
+    checkImages() {
+      const images = document.querySelectorAll(this.selector)
+      for (let i = this.indexNum; i < images.length; i++) {
+        if (this.isShow(images[i])) {
+          this.loadImage(images[i])
+          this.indexNum = i
+        }
+      }
+    }
+  }
+
+  return LazyLoad;
 }))
